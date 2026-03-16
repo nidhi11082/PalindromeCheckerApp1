e61b5e283d7c51b42815b3efdd440d693042d2e7
SUHANI SAHU <suhanisahi18@gmail.com>
2026-03-16 22:17:16 +0530

    Added Use Case 12 file

---
+++ src/UseCase12_StrategyPattern.java
@@ -0,0 +1,73 @@
+import java.util.Stack;
+import java.util.Deque;
+import java.util.LinkedList;
+
+// Strategy Interface
+interface PalindromeStrategy {
+    boolean checkPalindrome(String word);
+}
+
+// Stack Strategy Implementation
+class StackStrategy implements PalindromeStrategy {
+
+    public boolean checkPalindrome(String word) {
+
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : word.toCharArray()) {
+            stack.push(c);
+        }
+
+        for (int i = 0; i < word.length(); i++) {
+            if (word.charAt(i) != stack.pop()) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
+// Deque Strategy Implementation
+class DequeStrategy implements PalindromeStrategy {
+
+    public boolean checkPalindrome(String word) {
+
+        Deque<Character> deque = new LinkedList<>();
+
+        for (char c : word.toCharArray()) {
+            deque.addLast(c);
+        }
+
+        while (deque.size() > 1) {
+
+            if (deque.removeFirst() != deque.removeLast()) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
+public class UseCase12_StrategyPattern {
+
+    public static void main(String[] args) {
+
+        String word = "madam";
+
+        // Choose strategy dynamically
+        PalindromeStrategy strategy;
+
+        // Example: choose StackStrategy
+        strategy = new StackStrategy();
+
+        boolean result = strategy.checkPalindrome(word);
+
+        if (result) {
+            System.out.println(word + " is a palindrome");
+        } else {
+            System.out.println(word + " is not a palindrome");
+        }
+    }
+}
