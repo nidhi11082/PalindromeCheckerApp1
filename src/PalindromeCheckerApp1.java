6da277dd48c0804245c3c7d544d3149494646c92
SUHANI SAHU <suhanisahi18@gmail.com>
2026-03-16 19:52:08 +0530

    Added PalindromeCheckerApp1 welcome screen

---
+++ src/PalindromeCheckerApp1.java
@@ -0,0 +1,16 @@
+public class PalindromeCheckerApp1 {
+
+    public static void main(String[] args) {
+
+        // Welcome message
+        System.out.println("Welcome to Palindrome Checker Application");
+
+        // Application information
+        System.out.println("Application Name : Palindrome Checker");
+        System.out.println("Version : 1.0");
+
+        // Application flow message
+        System.out.println("Program is ready to check palindromes.");
+
+    }
+}
