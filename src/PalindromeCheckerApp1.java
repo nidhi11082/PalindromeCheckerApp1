c09a40d4a5c2e7be4953500183adfa033fcd740e
SUHANI SAHU <suhanisahi18@gmail.com>
2026-03-16 21:35:38 +0530

    Added Use Case 3 - Reverse string palindrome check

--- src/PalindromeCheckerApp1.java
+++ src/PalindromeCheckerApp1.java
@@ -2,19 +2,16 @@ public class PalindromeCheckerApp1 {
 
     public static void main(String[] args) {
 
-        // Hardcoded string
-        String word = "madam";
+        String word = "madam";   // original string
+        String reversed = "";    // reversed string
 
-        // Variable for reversed string
-        String reversed = "";
-
-        // Reverse the string
-        for(int i = word.length() - 1; i >= 0; i--) {
+        // Reverse the string using loop
+        for (int i = word.length() - 1; i >= 0; i--) {
             reversed = reversed + word.charAt(i);
         }
 
-        // Check if palindrome
-        if(word.equals(reversed)) {
+        // Compare original and reversed
+        if (word.equals(reversed)) {
             System.out.println(word + " is a palindrome");
         } else {
             System.out.println(word + " is not a palindrome");
